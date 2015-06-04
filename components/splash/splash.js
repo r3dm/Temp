c64a5b5b7aa27220a7c9183f1081cd228fc964f9
Harry Moreno <morenoh149@gmail.com>
2015-06-04 14:28:13 -0700

    click splash screen loads forecast view

--- components/splash/splash.js
+++ components/splash/splash.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Navigation } from 'react-router'
 
 let Splash = React.createClass({
+  mixins: [Navigation],
+
   render: function() {
     return (
-      <div className="splash-wrapper">
-        <img src="tempSun.png" id="sun-icon" />
+      <div className="splash-wrapper"
+           onClick={() => this.transitionTo('home')} >
+        <i className="wi wi-day-sunny sun-icon"></i>
         <h1 className="splash">
           temp
           <span className="splash-degree">&deg;</span>
