34881c02ad9d32ddd3050a77570fe29d72b122c2
Harry Moreno <morenoh149@gmail.com>
2015-06-03 17:09:33 -0700

    added fontawesome

--- components/header/header.js
+++ components/header/header.js
@@ -4,7 +4,7 @@ let Header = React.createClass({
   render() {
     return (
       <div className="header-wrapper">
-        <div className="settings-icon">*</div>
+        <i className="settings-icon fa fa-gears"></i>
         <p className="city-state">san francisco, ca</p>
         <p className="header-date">tuesday, june 26</p>
       </div>
