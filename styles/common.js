853902bacaa53362a176ff15de3170227b3821dc
Harry Moreno <morenoh149@gmail.com>
2015-06-08 16:38:11 -0700

    swap hex for hsla colors

--- styles/common.js
+++ styles/common.js
@@ -1,8 +1,10 @@
 let Common = {
-  splashRed: '#ff5136',
-  tempBlue: '#7ea4b3',
-  borderColor: '#c9e2eb',
-  altFontColor: '#4c869b'
+  splashRed: 'hsla(8, 100%, 61%, 1.0)',
+  tempBlue: 'hsla(197, 26%, 60%, 1.0)',
+  borderColor: 'hsla(196, 46%, 85%, 1.0)',
+  altFontColor: 'hsla(196, 34%, 45%, 1.0)',
+  red: 'hsla(8, 100%, 61%, 1.0)',
+  blue: 'hsla(197, 26%, 60%, 1.0)'
 }
 
 export default Common
