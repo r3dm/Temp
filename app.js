810a6564d6975a362ddc3099132c3b0c0572580a
Harry Moreno <morenoh149@gmail.com>
2015-06-10 13:46:08 -0700

    consolodate app.js files

--- app.js
+++ app.js
@@ -2,17 +2,33 @@ require('./styles/style.styl')
 
 import React from 'react'
 import Router from 'react-router'
-import { Route, DefaultRoute } from 'react-router'
-
-import App from './components/app.js'
+import { Route, DefaultRoute, RouteHandler } from 'react-router'
 import Home from './components/home/home.js'
 import Splash from './components/splash/splash.js'
 // import Settings from './components/settings.js'
+import fetchWeather from './utils/weather.js'
+
+/*
+ * we let state reside in App so async weather can be fetched on the splash
+ * screen. This way when the user visits home we likely already have the info
+ */
+let App = React.createClass({
+  getInitialState() {
+    return { temp: Number.NaN }
+  },
+  componentDidMount() {
+    fetchWeather((result) => {
+      this.setState({ temp: Math.round(result.body.main.temp) })
+    })
+  },
+  render() {
+    return <RouteHandler temp={ this.state.temp } />
+  }
+})
 
 let routes = (
   <Route handler={App} path="/">
     <DefaultRoute handler={Splash} />
-
     <Route handler={Home}
            name="home"
            path="/home" />
